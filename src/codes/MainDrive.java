752bc6c1b6bc70c67ce2a2444d608973ad135d00
kwon <dhgns1013@naver.com>
2020-07-02 12:37:36 +0900

    메뉴 띄우기
    무한으로 입력

--- src/codes/MainDrive.java
+++ src/codes/MainDrive.java
@@ -1,9 +1,35 @@
 package codes;
 
+import java.util.Scanner;
+
 public class MainDrive {
 	
 	public static void main(String[] args) {
-		//git과 연동해서 전화번호부 만들 예정
+	
+	//git과 연동해서 전화번호부 만들 예정
+	
+	printMenu();
+	
 	}
 
+	public static void printMenu() {
+		
+	Scanner myScan = new Scanner(System.in);
+	
+	while (true) {
+		
+		System.out.println("*******전화번호부********");
+		System.out.println("1) 전화번호 추가 등록");
+		System.out.println("2) 전체 전화번호 목록 조회");
+		System.out.println("0) 프로그램 종료");
+		System.out.println("======================");
+		
+		
+		System.out.println("메뉴 선택: ");
+		int inputMenu = myScan.nextInt();
+		
+		
+	}
+		
+	}
 }
