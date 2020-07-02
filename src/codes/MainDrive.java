5d0010629b122d1ae41e76eba7a04bd1a2b2d889
kwon <dhgns1013@naver.com>
2020-07-02 15:00:56 +0900

    사용자 정보 출력 양식 변경

--- src/codes/MainDrive.java
+++ src/codes/MainDrive.java
@@ -1,5 +1,13 @@
 package codes;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Calendar;
 import java.util.Scanner;
 
 public class MainDrive {
@@ -36,6 +44,7 @@ public class MainDrive {
 			addPhoneNumToFile();
 			
 		}else if (inputMenu == 2) {
+			readAllPhoneNum();
 			
 		}else {
 			System.out.println("잘못된 입력입니다");
@@ -43,7 +52,50 @@ public class MainDrive {
 		}
 		
 	}
+	
+	
+	
+	
+	}public static void readAllPhoneNum() {
+		File file = new File("phoneBook.txt");
 		
+		try {
+			FileReader fr = new FileReader(file);
+			
+			BufferedReader br = new BufferedReader(fr);
+			
+			while (true) {
+				String line = br.readLine();
+				if (line == null) {
+					
+					System.out.println("연락처를 모두 읽었다");
+				
+					break;}
+//				} System.out.println(line);
+				
+				String[] userInfos = line.split(",");
+				
+				String userName = userInfos[0];
+				String userPhoneNum = userInfos[1];
+				
+				int userBirthYear = Integer.parseInt(userInfos[2]);
+				
+				Calendar now = Calendar.getInstance();
+				
+				int userAge = now.get(Calendar.YEAR) - userBirthYear + 1;
+			    String userInfoMessage = String.format("%s(%d세) : %s", userName, userAge, userPhoneNum);
+                System.out.println(userInfoMessage);
+			}
+			br.close();
+			fr.close();
+			
+		} catch (FileNotFoundException e) {
+			System.out.println("불러올 파일이 없다");
+			System.out.println("연락처를 저장하고 다시 시도해주세요");
+		} catch (IOException e) {
+			System.out.println("연락처를 읽어오는 중에 문제가 발생했다");
+			e.printStackTrace();
+		}
 	}
 	
 	public static void addPhoneNumToFile() {
@@ -58,6 +110,26 @@ public class MainDrive {
 		
 		System.out.println("생년 입력: ");
 	    int birthYear = myScan.nextInt();
+	    //파일명 설정
+	    File phoneBookFile = new File("phoneBook.txt");
+	    //기존 내용에 이어붙이기
+	    try {
+			FileWriter fw = new FileWriter(phoneBookFile, true);
+			
+			BufferedWriter bw = new BufferedWriter(fw);
+			
+			String infoStr = String.format("%s, %s, %d", name, phoneNum, birthYear);
+			
+					bw.append(infoStr);
+					
+					bw.newLine();
+			
+					bw.close();
+					fw.close();
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+		}
 	}
 	
 }
